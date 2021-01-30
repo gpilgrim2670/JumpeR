100e732913b972ff2c224433ca81b1f4aa1d78d4
Greg Pilgrim <gpilgrim2670@gmail.com>
2021-01-30 14:45:27 -0500

    added diamond league with birthdates to testing

--- tests/testthat/test-flash_parse_works.R
+++ tests/testthat/test-flash_parse_works.R
@@ -81,8 +81,6 @@ test_that("flash parse works pole vault", {
 
 })
 
-
-
 test_that("flash parse works relay, team scores in results", {
   file <-
     "https://www.flashresults.com/2018_Meets/Outdoor/05-05_A10/032-1.pdf"
@@ -95,6 +93,37 @@ test_that("flash parse works relay, team scores in results", {
                               Finals_Result = c("3:09.10", "3:09.15", "3:09.95", "3:13.42", "3:14.87", "3:16.06", "3:18.64", "3:20.43", "3:21.07", "DNF"),
                               DQ = c(rep(0, 9), 1),
                               Event = rep("Men 4x400 M Relay 2:55 PM 6 May 2018", 10))
+
+    # generate test df
+    df_test <-
+      tf_parse(
+        read_results(file),
+        attempts = TRUE,
+        attempts_results = TRUE,
+        relay_athletes = TRUE
+      )
+
+    # test
+    expect_equivalent(df_standard,
+                      df_test)
+  }
+})
+
+test_that("flash parse works sprint, diamond league with some birthdates", {
+  file <-
+    "https://www.flashresults.com/2019_Meets/Outdoor/06-30_PreClassic/002-1.pdf"
+  if (is_link_broken(file) == TRUE) {
+    warning("Link to external data is broken")
+  } else {
+    # generate standard
+    df_standard <- data.frame(Place = as.character(seq(1, 6, 1)),
+                              Name = c("Arian SMITH", "Marcellus MOORE", "Ryan MARTIN", "Ryan MULHOLLAND", "Lance BROOME", "Tyrese COOPER"),
+                              Age = c(NA, NA, "21-Sep-2001", NA, NA, "21-Mar-2000"),
+                              Team = rep("USA", 6),
+                              Finals_Result = c("10.41", "10.46", "10.56", "10.60", "10.63", "10.64"),
+                              DQ = rep(0, 6),
+                              Event = rep("Boys 100 M High School 12:46 30 Jun 2019", 6))
+    df_test[,5]
 
     # generate test df
     df_test <-
