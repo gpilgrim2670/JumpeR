4a20d6cb9145ac6dfa94599ecfc929ab2ff3c4ff
Greg Pilgrim <gpilgrim2670@gmail.com>
2021-04-06 11:28:33 -0400

    updates to event selectors inside cleaning function

--- R/flash_clean_events_helper.R
+++ R/flash_clean_events_helper.R
@@ -27,8 +27,9 @@ flash_clean_events_helper <-
                           "Javelin",
                           "Hammer",
                           "Weight")
-    distance_names <- c("^\\d?[5-9|0]00m", "^\\d?[5-9|0]00 M")
-    sprint_names <- c("^[1-4]?[0-9]0m", "^[1-4]?[0-9]0 M")
+    distance_names <- c("^\\d?[5-9|0]\\d0m", "^\\d\\d\\d0m", "^\\d?[5-9|0]\\d0 M", "^\\d\\d\\d0 M", " \\d?[5-9|0]\\d0m", " \\d\\d\\d\\d0m", " \\d?[5-9|0]\\d0 M", " \\d\\d\\d0 M", "Mile", "Race Walk", "\\d{5}")
+    sprint_names <- c("^[1-4]?[0-9]\\dm", "^[1-4]?[0-9]\\d M", " [1-4]?[0-9]\\dm", " [1-4]?[0-9]\\d M")
+    relay_names <- c("Relay", "relay")
 
     if (any(str_detect(event_name_helper, vertical_names)) == TRUE) {
       df_helper <- df_helper %>%
@@ -42,6 +43,8 @@ flash_clean_events_helper <-
     } else if (any(str_detect(event_name_helper, sprint_names)) == TRUE) {
       df_helper <- df_helper %>%
         flash_clean_sprint_events()
+    } else if (any(str_detect(event_name_helper, relay_names)) == TRUE) {
+      df_helper <- df_helper
     } else {
       warning(
         paste0(
