db232b0d4ee74cb8f6195a653ab5b1146220873d
Greg Pilgrim <gpilgrim2670@gmail.com>
2021-07-12 16:19:28 -0400

    rename distance medley to dmr

--- R/flash_event_parse.R
+++ R/flash_event_parse.R
@@ -73,7 +73,8 @@ flash_event_parse <- function(text){
     stringr::str_replace("(\\d)\\s[M|m](eter)? Hurdles$", "\\1m Hurdles") %>%
     stringr::str_replace("(\\d)\\s[M|m](eter)? Relay$", "\\1m Relay") %>%
     stringr::str_replace("1 Mile", "Mile") %>%  # reformat mile event name
-    stringr::str_remove("(Women )|(Men )|(Boys )|(Girls)|(Mixed )")
+    stringr::str_remove("(Women )|(Men )|(Boys )|(Girls)|(Mixed )") %>%
+    stringr::str_replace("Distance Medley", "Dmr")
     # stringr::str_replace("(\\d)0000m$", "\\10000m Race Walk") # name race walks
 
   return(event_name)
